3fc35c652fdf5296a95aa75908d832d22d56f69d
Seakorv <arttudun@gmail.com>
2025-11-03 14:48:38 +0200

    Buttonien teko aloitettu

--- Assets/Scripts/AnswerButtonScript.cs
+++ Assets/Scripts/AnswerButtonScript.cs
@@ -1,19 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AnswerButtonScript : MonoBehaviour
 {
     [SerializeField] private int myNumber;
-    [SerializeField] private string myName; //Väliaikainens
+    [SerializeField] private string myName; //Väliaikainen
     [SerializeField] private Button myButton;
+    [SerializeField] private TextMeshProUGUI myText;
 
     //[SerializeField] private  myScaleName;
-    public bool isCorrect {get; set;} = false;
-    // Update is called once per frame  
+    public bool IsCorrect { get; set; } = false;
+
+    void Start()
+    {
+        SetButtonName();
+        SetButtonText();
+        Debug.Log(myButton.name);
+        myButton.onClick.AddListener(TaskOnClick);
+    }
+    // Update is called once per frame
     void Update()
     {
-
+        
     }
+
+    public void TaskOnClick()
+    {
+        Debug.Log("Painoin nappulaa");
+    }
+
+    public void SetButtonName()
+    {
+        myButton.name = myName;
+    }
+
+    public void SetButtonText()
+    {
+        myText.text = $"Asteikko {myName}";
+    }
+
+
 }
